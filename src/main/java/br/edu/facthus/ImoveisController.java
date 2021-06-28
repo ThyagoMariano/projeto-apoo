90177a8b8147a81477fe49a88e7ad8012a8f4c7e
ThyagoMariano <74057836+ThyagoMariano@users.noreply.github.com>
2021-06-28 20:42:45 -0300

    T09 Gui Imoveis: Cadastrar e listar. (Falta completar parte back end)

--- src/main/java/br/edu/facthus/ImoveisController.java
+++ src/main/java/br/edu/facthus/ImoveisController.java
@@ -1,6 +1,31 @@
 package br.edu.facthus;
 
+import java.util.ArrayList;
+
+import br.edu.facthus.dao.ImovelDAO;
+import br.edu.facthus.model.Imovel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 // Tarefa 09: completar...
+
 public class ImoveisController {
 
+	@FXML
+	private TextField txtRegistro;
+	@FXML
+	private TextField txtAluguel;
+	@FXML
+	private TextField txtProprietario;
+	@FXML
+	private TableView<Imovel> tblImovel;
+	@FXML
+	private TableColumn<Imovel,String> cRegistro;
+	@FXML
+	private TableColumn<Imovel,String> cProprietario;
+	@FXML
+	private TableColumn<Imovel, Double> cAluguel;
+	
+	private ArrayList<Imovel> imovel = new ArrayList<>();
 }
